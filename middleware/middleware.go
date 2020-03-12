9ab622e56abc807c7ee2a5748e50b2042b5460ae
angus <xiaka53@vip.qq.com>
2020-03-12 21:01:02 +0800

    白名单过滤增加*

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -39,7 +39,7 @@ func IPAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		isMatched := false
 		for _, host := range lib.GetStringSliceConf("base.http.allow_ip") {
-			if c.ClientIP() == host {
+			if c.ClientIP() == host || host == "*" {
 				isMatched = true
 			}
 		}
